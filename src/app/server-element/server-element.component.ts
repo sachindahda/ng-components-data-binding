45765389cf53b676b95d0db700619a9c2dab34c6
Sachin Dahda <sachin.luminoguru@gmail.com>
2019-08-21 12:10:09 +0530

    life cycle hook new updates

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -10,7 +10,10 @@ import {
   AfterContentChecked,
   AfterViewInit,
   AfterViewChecked,
-  OnDestroy
+  OnDestroy,
+  ElementRef,
+  ViewChild,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -19,9 +22,11 @@ import {
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragrph: ElementRef;
   constructor() {
     console.log('construct');
   }
@@ -32,18 +37,24 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngOnInit() {
     console.log('ngoninit');
+    console.log('textcontent', this.header.nativeElement.textContent);
+    console.log('textcontent of paragraph', this.paragrph.nativeElement.textContent);
   }
   ngDoCheck() {
     console.log('ngDOCHeck');
   }
   ngAfterContentInit() {
     console.log('AfterContentInit');
+    console.log('textcontent of paragraph', this.paragrph.nativeElement.textContent);
+
   }
   ngAfterContentChecked() {
     console.log('ngAfterContentChecked');
   }
   ngAfterViewInit() {
     console.log('ngAfterViewInit');
+    console.log('textcontentAfter', this.header.nativeElement.textContent);
+
   }
   ngAfterViewChecked() {
     console.log('ngAfterViewChecked');
